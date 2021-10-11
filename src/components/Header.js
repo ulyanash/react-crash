d3d6468421efca6879cc257040f03d484a22a1dd
Ulya Sh <ulyana.sh@ramotion.agency>
2021-10-11 18:24:48 +0300

    add button component

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log('Click')
+  }
+
+  return (
+    <header className='header'>
+      <h1>{ title }</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+    </header>
+  )
+}
+
+// css in js
+// const headingStyle = {
+//   color: 'red', backgroundColor: 'black'
+// }
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
